14250d6c980866e41c61d4df2107b04da7c37b9d
Katya Raman <katyaraman@icloud.com>
2025-04-16 18:02:57 -0700

    first commit

---
+++ Fetch Apprenticeship Challenge 2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fetch Apprenticeship Challenge 2025
+//
+//  Created by Katyayani G. Raman on 4/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fetch Apprenticeship Challenge 2025/Fetch_Apprenticeship_Challenge_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Apprenticeship_Challenge_2025App.swift
+//  Fetch Apprenticeship Challenge 2025
+//
+//  Created by Katyayani G. Raman on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Apprenticeship_Challenge_2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
